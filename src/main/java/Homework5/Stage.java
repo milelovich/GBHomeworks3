43028b394815ea7e8bd53c4e9503ad115b15cfd0
milelovich <milelovich@mail.ru>
2021-05-02 08:52:20 +0300

    Finished Homework5

---
+++ src/main/java/Homework5/Stage.java
@@ -0,0 +1,12 @@
+package Homework5;
+
+public abstract class Stage {
+    int length;
+    String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract void go(Car c);
+}
